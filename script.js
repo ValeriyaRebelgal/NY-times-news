d5e8bde2fcbea05748157a25e28422aef665afeb
Valeriya <valeritapetrova@gmail.com>
2017-06-19 20:43:05 +0300

    Update script.js

--- script.js
+++ script.js
@@ -7,23 +7,28 @@
 var URL = 'https://newsapi.org/v1/articles?source=the-new-york-times&sortBy=top&apiKey=23227d1cf38c49f98e701918f0d46fe4';
 var nyContainer = document.getElementById("ny-info");
 var btn = document.getElementById("btn");
+
+/**
+* Add Source Selector with an Id
+* Add Sort By Selector with an Id
+*/
+var source = document.getElementById('source');
+var sortBy = document.getElementById("sortBy");
+
 /**
 * Get url
 * Callback
 */
 btn.onclick = function() {
-    ajax(URL, renderHTML); 
+  var url = 'https://newsapi.org/v1/articles?source='+ source.value +'&sortBy='+ sortBy.value +'&apiKey=23227d1cf38c49f98e701918f0d46fe4';
+    ajax(url, renderHTML); 
+    document.getElementById('source').value;
+    document.getElementById('sortBy').value;
   };
 
   function ajax(url, callback) { 
     var myRequest = new XMLHttpRequest();
     myRequest.open('GET', url);
-
-    myRequest.onreadystatechange = function() {
-        if(myRequest.status === 200) {
-          callback(myRequest.responseText);
-        }
-    };
 
     myRequest.onload = function() {
       var myData = JSON.parse(myRequest.responseText);
@@ -35,11 +40,12 @@ btn.onclick = function() {
 
 /**
 * iterate throw articles to generate html
-* @param {object} data - response
-* @param {object} data.articles - response
-* @param {object} data.image - response
-* @param {object} data.date - response
-*/
+* @param {object} data
+* @param {Array} data.articles
+* @param {string} data.sortBy
+* @param {string} data.source
+* @param {string} data.status
+**/
 
 function renderHTML(data) {
   var content = '';
@@ -48,22 +54,28 @@ function renderHTML(data) {
   //iteration throw news list
   data.articles.forEach(function (article){
     //article object
-    //Images in API js
-    var img = new Image(article.images);
-    img.src = URL;
     //Date in API js
     var date = new Date(article.publishedAt);
+    var formatedDate = getFormatedDate(date);
 
     content += "<div class='article'>"; //parent
-    content += "<a href='"+ article.url +"'>" + article.title +"</a>";
-    content += "<img>"+ article.images +"</img>";
-    content += "<p>" + article.description + "</p>";
-    content += "<h4>" + article.author + "</h4>";
-    content += "<p>" +article.publishedAt + "</p>";
+    content += "<a class='article-title' href='"+ article.url +"'>" + article.title +"<br><br>"+"</a>";
+    content += "<img class='article-img' src='"+article.urlToImage+"' alt='"+article.title+"<br><br>"+"'>";
+    content += "<p class='article-publishedAt'>" + formatedDate + "<br><br>"+ "</p>";
+    content += "<p class='article-description'>" + article.description + "<br><br>"+"</p>";
+    content += "<span class='article-author'>" +article.author + "<br><br>"+"</span>";
+    content += "<hr>";
+    content += "<br>";
     content += "</div>";
   });
-
- 
   nyContainer.innerHTML = content;
 }
+/** Change date format and display changes
+*/
+function getFormatedDate(date) {
+var formatedDate = new Date(); 
+return formatedDate;
+}
+
+
 
